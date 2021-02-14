12aeb604c768242b204e229acd785d6d023c9694
Paul Yim <paul.j.yim@gmail.com>
2021-02-14 05:27:56 +0000

    Add bootstrapping logic; needs to look in /usr/lib64 and /usr/include for system deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
+  export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+else
+  export CFLAGS="${CFLAGS} -I${PREFIX}/include -I/usr/include"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -L/usr/lib64"
+fi
+
 set -ex
 
 ./configure --prefix=${PREFIX} \
             --host=${HOST} \
-            --disable-static \
-            CFLAGS="${CFLAGS} -I${PREFIX}/include" \
-            LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+            --disable-static
+
 make -j ${CPU_COUNT} ${VERBOSE_AT}
 make install
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-    make check
+  make check
 fi
+
 # This conflicts with a file in readline
 rm -f ${PREFIX}/share/man/man3/history.3
